50db09d7339c8b0a4b98beb9d2f6bb8bc35958c4
Connor Keevill <connor.keevill.19@gmail.com>
2023-07-27 22:29:01 +0100

    Add helper function which can output a MNIST vector as a ppm file

--- helpers.h
+++ helpers.h
@@ -1,4 +1,7 @@
 #include <random>
+#include <iostream>
+#include <fstream>
+#include <vector>
 
 double rand(double lower, double upper) {
     // Create a random number generator engine
@@ -10,4 +13,45 @@ double rand(double lower, double upper) {
 
     // Generate a random number using the distribution and generator
     return distribution(gen);
+}
+
+/**
+ * Writes the given feature vector (of greyscale pixels values) to a .ppm file.
+ * Note that the pixel values should be on the interval [0, 1], as if neuron activations.
+ *
+ * @param pixels the pixels in the feature vector
+ * @param width the width of the image
+ * @param height the height of the image
+ * @param filename the filename to write the image to; should have .ppm extension
+ */
+void WriteFeatureVectorAsImage(const std::vector<double>& pixels, int width, int height, const std::string& filename) {
+
+	// Check if the vector size matches the specified dimensions (28x28 = 784)
+    if (pixels.size() != width * height) {
+        std::cerr << "Invalid input vector size. Expected 784 elements (28x28 pixels)." << std::endl;
+        return;
+    }
+
+    // Open the file in binary mode for writing
+    std::ofstream file(filename, std::ios::binary);
+
+    if (!file) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return;
+    }
+
+    // Write the PPM header
+    file << "P5\n" << width << " " << height << "\n255\n";
+
+    // Write the pixel values to the file
+	for(double P : pixels)
+	{
+		P *= 255; // Multiply to scale up to correct value.
+		file << (unsigned char) P;
+	}
+
+    // Close the file
+    file.close();
+
+    std::cout << "Image data saved to: " << filename << std::endl;
 }
