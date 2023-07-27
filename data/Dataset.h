dc14d19b697a9e5886e91ff2b5a014048b80bbf3
Connor Keevill <connor.keevill.19@gmail.com>
2023-07-27 21:31:30 +0100

    Implement Dataset class and create new MnistDataset class

--- data/Dataset.h
+++ data/Dataset.h
@@ -1,15 +1,38 @@
-//
-// Created by Connor Keevill on 26/07/2023.
-//
+#pragma once
 
-#ifndef NEURAL_NETWORK_DATASET_H
-#define NEURAL_NETWORK_DATASET_H
+#include <string>
+#include <vector>
 
+using namespace std;
+
+struct FeatureVector
+{
+	vector<double> data;
+	vector<double> label;
+};
 
 class Dataset
 {
+public:
+	Dataset();
 
+	FeatureVector GetNextFeatureVector();
+	bool EndOfData();
+	void ResetCounter();
+protected:
+	vector<vector<double>> data;
+	vector<vector<double>> labels; // One-hot encoded vector of the label. Might make sense for this to be int or even
+								   // bool, but double is in fact needed for backprop (specifically to create the cost
+								   // function).
+	int counter;
 };
 
-
-#endif //NEURAL_NETWORK_DATASET_H
+// See: http://yann.lecun.com/exdb/mnist/
+class MnistDataset : public Dataset
+{
+public:
+	explicit MnistDataset(const string& dataFilepath, const string& labelsFilepath);
+private:
+	static vector<vector<double>> ReadDataset(const string& filepath);
+	static vector<vector<double>> ReadLabels(const string& filepath);
+};
