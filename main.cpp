deb58d1d48c56c7af0fec59af334bfae34ce27cd
Connor Keevill <connor.keevill.19@gmail.com>
2023-07-27 21:51:35 +0100

    Write quick test that network can perform forward pass properly. It can!

--- main.cpp
+++ main.cpp
@@ -5,10 +5,30 @@
 int main()
 {
 	// Create a network with 3 input neurons, one hidden layer with 3 neurons, and an output layer of 2 neurons.
-	auto network = std::make_unique<MultilayerPerceptron>(std::vector<int>{3, 3, 2}, [](double x) -> double {
+	auto network = std::make_unique<MultilayerPerceptron>(std::vector<int>{784, 3, 10}, [](double x) -> double {
         return 1.0 / (1.0 + std::exp(-x));
     });
-	auto trainingData = std::make_unique<Dataset>();
 
-	network->Train(*trainingData);
+	Dataset data = MnistDataset("t10k-images-idx3-ubyte", "t10k-labels-idx1-ubyte");
+
+	for(int i = 0; i < 5; ++i)
+	{
+		FeatureVector fv = data.GetNextFeatureVector();
+		vector<double> out = network->ForwardPass(fv.data);
+
+		for(double val : out)
+		{
+			cout << val << " ";
+		}
+
+		cout << endl;
+
+		for(double val : fv.label)
+		{
+			cout << val << " ";
+		}
+
+		cout << endl;
+		cout << endl;
+	}
 }
