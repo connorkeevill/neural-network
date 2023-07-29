eab499f7c309e9f40583cbe896c562fdd017d71c
Connor Keevill <connor.keevill.19@gmail.com>
2023-07-29 20:33:25 +0100

    Create new ActivationFunction class with Sigmoid child to allow for activation function derivative

---
+++ ActivationFunction.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include "ActivationFunction.h"
+
+
+/**
+ * @brief Sigmoid function.
+ *
+ * This function calculates the sigmoid value for the given input.
+ *
+ * @param input The input value.
+ * @return The sigmoid value.
+ */
+double Sigmoid::Function(double input)
+{
+	return 1.0 / (1.0 + std::exp(-input));
+}
+
+
+/**
+ * Calculates the derivative of the sigmoid function at a given input.
+ *
+ * @param input The input value to the sigmoid function.
+ * @return The derivative value of the sigmoid function at the given input.
+ */
+double Sigmoid::Derivative(double input)
+{
+	return input * (1 - input);
+}
+
+
---
+++ ActivationFunction.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class ActivationFunction
+{
+public:
+	virtual double Function(double input) = 0;
+	virtual double Derivative(double input) = 0;
+    virtual ~ActivationFunction() = default;
+};
+
+class Sigmoid : public ActivationFunction
+{
+public:
+	double Function(double input) override;
+	double Derivative(double input) override;
+}
