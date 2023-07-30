cf70c0edc67871f473d26e92d18ba62b9c82155e
Connor Keevill <connor.keevill.19@gmail.com>
2023-07-30 21:03:18 +0100

    Create functions in dataset to convert a vector output from the network into the actual class which is predicted

--- data/Dataset.h
+++ data/Dataset.h
@@ -19,6 +19,7 @@ public:
 	FeatureVector GetNextFeatureVector();
 	bool EndOfData();
 	void ResetCounter();
+	virtual string ClassificationToString(vector<double> output) { return ""; }
 protected:
 	vector<vector<double>> data;
 	vector<vector<double>> labels; // One-hot encoded vector of the label. Might make sense for this to be int or even
@@ -32,6 +33,7 @@ class MnistDataset : public Dataset
 {
 public:
 	explicit MnistDataset(const string& dataFilepath, const string& labelsFilepath);
+	string ClassificationToString(vector<double> output);
 private:
 	static vector<vector<double>> ReadDataset(const string& filepath);
 	static vector<vector<double>> ReadLabels(const string& filepath);
